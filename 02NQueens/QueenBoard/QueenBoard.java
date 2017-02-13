b01cda4f3d8f3a7269c35c19b0989111f2eb9f09
QuillChen50 <jchen50@stuy.edu>
2017-02-13 03:37:31 -0500

    working on solveH

---
+++ 02NQueens/QueenBoard/QueenBoard.java
@@ -0,0 +1,34 @@
+public class QueenBoard{
+    private int[][]board;
+    private int solutionCount;
+    private int s;
+    
+    public QueenBoard(int size){
+	board = new int[size][size];
+	s = size;
+    }
+
+    public int getSize(){
+        return s;
+    }
+
+    public boolean solve() 
+    {
+	return solveH(0);
+    }
+    }
+
+    private boolean solveH(int col){
+	int[][] temp == new int[getSize()][getSize()];
+	int rowlength == 
+	int r == 0;
+	if (int[r][col] == 0){
+	    int[r][col] == -1;
+	    for (int i = 0; 
+	    return true;
+	    
+		    
+	return false;
+    }
+
+}
