de3e4fc608fa0382aa8e76a03d90db8808cd2e25
Jenny Chen <jchen50@stuy.edu>
2017-03-12 22:51:00 -0400

    Will come back to this later

--- 05LabStuff/USACO.java
+++ 05LabStuff/USACO.java
@@ -1,20 +1,22 @@
 import java.io.*;
 import java.util.*;
 public class USACO{
-    // private int[][]bronzePasture;
+    private int[][]bronzePasture;
     private char[][]silverPasture;
-    // private int finalElevation;
+    private int finalElevation;
     private int time;
+    private int smallest;
     
   public void bronze(String filename){
       try {
 	  Scanner sc = new Scanner(new File(filename));
 	  int row = Integer.parseInt(sc.next());
+	  //System.out.println(row);
 	  int col = Integer.parseInt(sc.next());
 	  bronzePasture = new int[row][col];
 	  finalElevation = Integer.parseInt(sc.next());
 	  int numInstructions = Integer.parseInt(sc.next());
-	  sc.nextLine();
+	  //sc.nextLine();
 	  for (int r = 0; r<row; r++){
 	      for(int c = 0; c<col;c++) {
 		  bronzePasture[r][c] = Integer.parseInt(sc.next());
@@ -35,12 +37,24 @@ public class USACO{
 
   }
     private void cowStomping(int row, int column, int depth){
-	System.out.println(row);
-	System.out.println(column);
-	System.out.println(depth);
+	System.out.print(row + ", ");
+	System.out.print(column + ", ");
+	System.out.print(depth + "\n");
+	
+	    bronzePasture[row-1][column-1]
+	}
+	    
 	
 	
       }
+
+    public void min (int a, int b, int c){
+	
+        smallest = Math.min(Math.min(a, b), c);
+    }
+
+
+	
 
     //still trying to get silver's scanner to work  
     public void silver(String filename){
@@ -78,7 +92,7 @@ public class USACO{
 
     public static void main(String[]args){
 	USACO x = new USACO();
-	//x.bronze("BronzeTest.txt");
+	x.bronze("BronzeTest.txt");
 	//x.silver("SilverTest.txt");
     }
 }
