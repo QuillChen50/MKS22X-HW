9bfb1ad77734038321e64cd7f46231392431fa06
Jenny Chen <jchen50@stuy.edu>
2017-03-14 08:04:26 -0400

    Still need to resolve some repetitions

--- 06Combination/Quiz2Redux.java
+++ 06Combination/Quiz2Redux.java
@@ -8,41 +8,62 @@ public class Quiz2Redux{
     public static ArrayList<String> combinations(String s){
 	String result = s;
 	ArrayList<String>words = new ArrayList<String>();
-	help( words , result, "" , 0);
+	words.add("");
+	help( words , result);
 	Collections.sort(words);
 	return words;
     }
   
-    private static void help( ArrayList<String> words,String in, String out, int index){
-	for (int i = index; i < in.length(); i++){
-	    out += in.charAt(i);
+    private static void help( ArrayList<String> words,String in){
+	/*	if (in.length() == 1){
+		words.add(in);
+		return;}
+	*/
+	if (in.length() == 0){
+	    return;}
+	String out = "" + in.charAt(0);
+	words.add(out);
+	for (int j = 1; j < in.length(); j++){
+	    /*System.out.println("added");
+	      for (String str : words){
+	      System.out.println( str);}
+	    */
+	    System.out.println(j);
+	    out = out + in.charAt(j);
 	    words.add(out);
-	    System.out.println(words.get());
-	    help(words, in, out, i+1);
-	    //out.remove(out.length() - 1);
-	    removeCharAt(out, out.length() - 1);
-	}
-    }
 
-    public static String removeCharAt(String s, int pos) {
-      return s.substring(0, pos) + s.substring(pos + 1);
-   }
+	    help(words, in.substring(1));
+	    for (int i = j; i < 0; i++){
+	    removeCharAt(out, out.length() - 1);
+	    }
+	}
+	    
+	for (String str : words){
+	    System.out.println( str);}
+    
+}
+
+public static String removeCharAt(String s, int index) {
+    return s.substring(0, index) + s.substring(index + 1);
+}
+
+	
 			      
 			   
-	/*
-	for (int i = 0; i < result.length(); i++) {
-	    // Record size as the list will change
-	    int ArrayLength = words.size();
-	    for (int j = 0; j < ArrayLength; j++) {
-	        words.add(s.charAt(i) + words.get(j));
-	    }
-	    words.add(Character.toString(s.charAt(i)));
-	}
-	METHOD TO BE WRITTEN BY YOU.*/
+/*
+  for (int i = 0; i < result.length(); i++) {
+  // Record size as the list will change
+  int ArrayLength = words.size();
+  for (int j = 0; j < ArrayLength; j++) {
+  words.add(s.charAt(i) + words.get(j));
+  }
+  words.add(Character.toString(s.charAt(i)));
+  }
+  METHOD TO BE WRITTEN BY YOU.*/
 	
     
 
-    public static void main (String[] args){
-	combinations("abc");
-    }
+public static void main (String[] args){
+    System.out.println(combinations("abc"));
+}
 }
