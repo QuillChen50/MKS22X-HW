1af7bded3ddc43c8572ecb458d6832818724a6a8
Jenny Chen <jchen50@stuy.edu>
2017-03-14 00:24:06 -0400

    Rough start with Quiz2Redux. Will work on more in the morning

--- 06Combination/Quiz2Redux.java
+++ 06Combination/Quiz2Redux.java
@@ -1,22 +1,35 @@
 import java.util.*;
 public class Quiz2Redux{
-    private result = "";
     /*Returns an ArrayList<String> that contains all subsets of the
      *characters of String s. Assume s has no duplicate characters.
      *the characters should appear in the same order that they occur 
      *in the original string.
      */
     public static ArrayList<String> combinations(String s){
-	result = s;
+	String result = s;
 	ArrayList<String>words = new ArrayList<String>();
-	help( words , /*fill this in with more */);
+	help( words , result, "" , 0);
 	Collections.sort(words);
 	return words;
     }
   
-    private static void help( ArrayList<String> words, 
-			      /*fill this in with more arguments*/ ){
+    private static void help( ArrayList<String> words,String in, String out, int index){
+	for (int i = index; i < in.length(); i++){
+	    out += in.charAt(i);
+	    words.add(out);
+	    System.out.println(words.get());
+	    help(words, in, out, i+1);
+	    //out.remove(out.length() - 1);
+	    removeCharAt(out, out.length() - 1);
+	}
+    }
 
+    public static String removeCharAt(String s, int pos) {
+      return s.substring(0, pos) + s.substring(pos + 1);
+   }
+			      
+			   
+	/*
 	for (int i = 0; i < result.length(); i++) {
 	    // Record size as the list will change
 	    int ArrayLength = words.size();
@@ -25,6 +38,11 @@ public class Quiz2Redux{
 	    }
 	    words.add(Character.toString(s.charAt(i)));
 	}
-	/*METHOD TO BE WRITTEN BY YOU.*/ 
+	METHOD TO BE WRITTEN BY YOU.*/
+	
+    
+
+    public static void main (String[] args){
+	combinations("abc");
     }
 }
