5d010e1b40f382c1e1d5d6f46f530758bb86113d
QuillChen50 <jchen50@stuy.edu>
2017-03-21 12:32:13 -0400

    Redoing part

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -1,21 +1,49 @@
+import java.util.*;
+
 public class Quick{
     
     private static final Random random = new Random();
 
-    public static int part (int[] data, int start, int end){
+    public static int part(int[] data, int start, int end){
+	int pivot = random.nextInt(data.length);
+    }
+
+
+    public static void selectH (int[]data, int k){
+
+    }
+
+
+    public static int quickselect (int[] data, int k){
+
+	int pivot = random.nextInt(data.length);
+int value = data[pivot]
+
+	/*
+//Rewriting this
 	//generate random index
-	index = random.nextInt(someArray.length);
-	int pivot = data[start];
+	int index = random.nextInt(data.length);
+	int pivot = data[index];
 	int[] temp = new int[data.length];
-	int back = 0
-	for (int i = 0; i < data.length, i++){
+
+	for (int i = 0; i < data.length; i++){
 	    if (data[i] <= pivot){
 		temp[i] = data[i];}
 	    else {
 		
 		temp[temp.length - back] = data[i];
-back++
 	    }
+	    //System.out.println(back);
+
+	}
+	return pivot;
+	*/
+    }
+
+	public static void main (String [] args){
+	    int[] array = {1, 2, 3, 5, 0};
+	    quickselect(array, 5);
+
 
 	}
 
