659f1037591b8bc8438700069b1c06e0dc3ca9df
Jenny Chen <jchen50@stuy.edu>
2017-04-20 06:17:04 -0400

    Will work on this after Quick is done.

---
+++ 08MergeSort/MergeSort.java
@@ -0,0 +1,9 @@
+//Will work on this after getting Quick done
+
+import java.util.Arrays;
+public class Merge{
+    
+    public static void mergesort(int[] ary){
+	mergesort(ary, 0, ary.length-1);
+    }
+}
