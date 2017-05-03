b478694bc5ba3f6026a3a872bd7e7fcba3e1c682
QuillChen50 <jchen50@stuy.edu>
2017-05-03 12:32:14 -0400

    started Heap class, add and remove

---
+++ 13MyHeap/MyHeap.java
@@ -0,0 +1,20 @@
+public class MyHeap{
+
+    private ArrayList<Integer> arr;
+
+    public MyHeap (ArrayList<Integer> a){
+	int s = a.size();
+	arr = new ArrayList<Integer>(s);
+	for (Integer n: a){
+
+    } 
+
+    public void add (Integer n){
+
+    }
+
+    public void remove (Integer n){
+
+    }
+
+}
