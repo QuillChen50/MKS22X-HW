bc11216f6f1902ea09a62778b01a0a7c9c7356d1
QuillChen50 <jchen50@stuy.edu>
2017-05-09 16:32:07 -0400

    Finished drafting code, still need to test and debug methods

--- 13MyHeap/MyHeap.java
+++ 13MyHeap/MyHeap.java
@@ -4,6 +4,7 @@ public class MyHeap{
     private int size;
     private int min;
 
+
     //arraylist.size is one larger 
 
     public MyHeap(){
@@ -11,50 +12,77 @@ public class MyHeap{
 	arr = new ArrayList<String>();
     }
 
+    //if min is true, then its a min heap
     public MyHeap(boolean mm){
 	int size = 0;
 	arr = new ArrayList<String>();
 	if (mm == true){
-	    min = -1;
+	    min = 1;
 	}
 	else{
-	    min = 1;
+	    min = -1;
 	}}
 
 
 
+
+
     public void add (String s){
-	arr.add(s);
-
-	if (index > arr[index/2]){
-
-	}
-
-
+	int index = arr.size();
+	arr.get(0) = arr.size() + 1;
+	arr.add(index, s);
+	pushUp(arr.size() - 1);
 
     }
 
-    public void remove (Integer n){
+    public String remove (){
+	String root = arr.get(1);
+	String last = arr.get(arr.size()-1);
+	arr.set(1, last);
+	pushDown(1);
 
     }
 
     //push up or down a index
     //parameter is index of child (value being pushed up or down)
 
-    private void pushUp(){
-	int parent = 1;
-	int child = 0;
-int m = arr.get(1);
-	for (int i = 2; i < arr.size(); i=i+2){
-	    if ( (arr.get(2i) < m*min) || (arr.get(2i+1) < m*min)){
-		if (arr.get(2i)*min < arr.get(21+1)*min){
-		    arr.set(2i, m);
-
-
+    private void pushUp(int index){
+	String child = arr[index];
+	String parent = arr[index/2];
+	//compareTo should have returned less than 0 if true
+	while (( (child.compareTo(parent))* 1.0 * min < 0)
+	    && index > 1){
+	    swap(index, index/2);
+	    index = index/2;
 	}
     }
 
-    private void pushDown(){
+    private void pushDown(int index){
+	String child = arr[index*2];
+	String parent = arr[index];
+	while ( ( (parent.compareTo(child))* 1.0 * min > 0)
+	     && ((index*2) <= arr.size())) {
+	    swap(index, index*2);
+	    index = index*2;
+	}
+    }
 
+    private void swap(int a, int b){
+	int temp = arr[a];
+	arr[a] = arr[b];
+	arr[b] = temp;
+    }
+
+    public String toString(){
+	String result = "";
+	for (String s : arr) {
+	    result = result + s;
+	}
+
+    public static void main(String [] args){
+	MyHeap h = new MyHeap(true);
+	h.add("hello");
+	h.add("hello2");
+	h.remove();
     }
 }
